d16cac6e85d24b83b5058aa697f95767114a86c8
maeil99 <ismailmohdnorit@gmail.com>
2022-10-13 16:15:55 +0800

    fix UI Element

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './Navbar' 
-import Footer from './Footer' 
 
 interface IPageProps{
     children: React.ReactNode;
@@ -10,7 +9,6 @@ const Layout = ({ children }:IPageProps) => (
   <>
     <Navbar />
     {children}
-    <Footer />
   </>
 );
 
